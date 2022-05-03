f51a1be123f4a8119dbf1262f351b4b33ee50eba
Hermes Júnior <hermesj.junior@gmail.com>
2022-05-03 13:45:18 -0300

    Ajuste dos endpoints dos participants

--- index.js
+++ index.js
@@ -11,36 +11,72 @@ app.use(json());
 app.use(cors());
 dotenv.config();
 
-let database = null;
+let db = null;
 const mongoClient = new MongoClient(process.env.MONGO_URI);
 const promise = mongoClient.connect();
 promise.then(() => {
-    database = mongoClient.db("batepapo-uol");
-    console.log(chalk.bold.green("Sua conexão com o banco de dados está de pé"));
+    db = mongoClient.db("batepapo-uol");
+    console.log(chalk.green.bold("Sua conexão com o banco de dados está de pé"));
 });
-promise.catch(e => console.log(chalk.bold.red("O banco de dados não aceitou sua conexão"), e));
+promise.catch(e => console.log(chalk.red.bold("O banco de dados não aceitou sua conexão"), e));
 
 
-app.post("/participants", (req, res) => {
-    const body = req.body;
+app.post("/participants", async (req, res) => {
+    let { name } = req.body;
+    const time = dayjs().format('hh:mm:ss');
+    let stop;
 
-    const datasUser = {
-        name: body.name,
-        lastStatus: body.lastStatus
-    };
-    const promise = database.collection("participants").insertOne(datasUser);
-    promise.then(() => {
+    const scheme = Joi.object().keys({
+        name: Joi.string().min(1).required()
+    });
+    const result = scheme.validate({name});
+    const { error } = result;
+    const valid = error == null;
+
+    if(!valid) {
+        res.status(422).send("Insira um nome válido");
+        return;
+    }
+
+    const listUsers = await db.collection("registeredUsers").find({}).toArray();
+    listUsers.map(user => {
+        if(user.name === name) {
+            stop = "stop";
+            res.status(409).send("Nome de usuário já existente. Utilize outro nome.");
+            return;
+        }
+    });
+
+    if(stop) return;
+    addUser(name, time, res);
+});
+
+function addUser(name, time, res) {
+    const user = {
+        name,
+        lastStatus: Date.now()
+    }
+    const status = {
+        from: name,
+        to: 'Todos',
+        text: 'entra na sala...',
+        type: 'status',
+        time
+    }
+    const promise = db.collection("registeredUsers").insertOne(user);
+    promise.then(async () => {
+        await db.collection("messagesUsers").insertOne(status);
         res.sendStatus(201);
     });
-    promise.catch(e => {
-        res.sendStatus(500);
-    })
-});
+    promise.catch(() => res.sendStatus(500));
+};
 
 app.get("/participants", (req, res) => {
-    const listaPartcipants = db.collection("participants").find({}).toArray();
-    res.send(listaPartcipants);
+    db.collection("registeredUsers").find({}).toArray().then(users => res.send(users));
 });
+
+
+
 
 app.post("/messages", (req, res) => {
     const body = req.body;
@@ -50,7 +86,7 @@ app.post("/messages", (req, res) => {
         text: body.text,
         type: body.type
     };
-    const promise = database.collection("messages").insertOne(datasMessage);
+    const promise = db.collection("messages").insertOne(datasMessage);
     promise.then(() => {
         res.sendStatus(201);
     });
@@ -70,7 +106,7 @@ app.post("/status", (req, res) => {
     const datasStatus = {
         name: body.name
     };
-    const promise = database.collection("status").insertOne(datasStatus);
+    const promise = db.collection("status").insertOne(datasStatus);
     promise.then(() => {
         res.sendStatus(201);
     });
